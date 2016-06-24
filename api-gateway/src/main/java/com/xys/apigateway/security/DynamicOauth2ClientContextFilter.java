3db28042e313c156e87411744410f3b1648ef218
摇光 <xieyushi@ancun.com>
2016-06-24 14:45:37 +0800

    oauth2.0 behind zuul

---
+++ api-gateway/src/main/java/com/xys/apigateway/security/DynamicOauth2ClientContextFilter.java
@@ -0,0 +1,54 @@
+package com.xys.apigateway.security;
+
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.resource.UserRedirectRequiredException;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * 动态OAuth2过滤器
+ *
+ * @author 摇光
+ * @version 1.0
+ * @Created on 2016/6/24
+ * @Copyright:杭州安存网络科技有限公司 Copyright (c) 2016
+ */
+public class DynamicOauth2ClientContextFilter extends OAuth2ClientContextFilter {
+
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+    @Override
+    protected void redirectUser(UserRedirectRequiredException e, HttpServletRequest request,
+                                HttpServletResponse response) throws IOException {
+        String redirectUri = e.getRedirectUri();
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(redirectUri);
+        Map<String, String> requestParams = e.getRequestParams();
+        for (Map.Entry<String, String> param : requestParams.entrySet()) {
+            builder.queryParam(param.getKey(), param.getValue());
+        }
+
+        if (e.getStateKey() != null) {
+            builder.queryParam("state", e.getStateKey());
+        }
+
+        String url = getBaseUrl(request) + builder.build().encode().toUriString();
+        this.redirectStrategy.sendRedirect(request, response, url);
+    }
+
+    @Override
+    public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
+        this.redirectStrategy = redirectStrategy;
+    }
+
+    private String getBaseUrl(HttpServletRequest request) {
+        StringBuffer url = request.getRequestURL();
+        return  url.substring(0, url.length() - request.getRequestURI().length() + request.getContextPath().length());
+    }
+
+}
