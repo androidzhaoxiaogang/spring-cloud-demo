9335f668edfa4f7cb24c1519e94dea21b7b9e587
摇光 <xieyushi@ancun.com>
2016-06-27 17:30:42 +0800

    添加服务注册

--- support/monitor-dashboard/src/main/java/com/xys/monitor/HystrixDashboardApplication.java
+++ support/monitor-dashboard/src/main/java/com/xys/monitor/HystrixDashboardApplication.java
@@ -3,6 +3,7 @@ package com.xys.monitor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @Copyright:杭州安存网络科技有限公司 Copyright (c) 2016
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableHystrixDashboard
 public class HystrixDashboardApplication extends SpringBootServletInitializer {
 
